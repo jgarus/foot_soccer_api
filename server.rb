eccbf8e49e09affa4b0ff729fa1c848603af7ea3
jgarus <javprg7@gmail.com>
2016-06-24 20:41:12 -0400

    Addin9

--- server.rb
+++ server.rb
@@ -6,8 +6,8 @@ get '/teams' do
   content_type :json
   jsonArr = {"teams":[{"name":"OSC Lille", "code":"OSC",
                         "shortname":"Lille", "marketValue":"67,700,000 e",
-                        "image":"/img/clubs/lille.jpg"},
+                        "image":"http://upload.wikimedia.org/wikipedia/de/0/08/LOSC_Lille_Crest_2012.png"},
                       {"name":"Paris Saint Germain", "code":"PSG",
                         "shortname":"PSG", "marketValue":"408,550,000 e",
-                        "image":"/img/clubs/psg.png"}]}.to_json
+                        "image":"http://upload.wikimedia.org/wikipedia/de/1/1e/Paris_Saint-Germain_(seit_2013).gif"}]}.to_json
 end
