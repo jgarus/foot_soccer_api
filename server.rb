2272c5241d6b328ae2d38abafef75c85a5aee899
jgarus <javprg7@gmail.com>
2016-06-24 22:48:38 -0400

    Addin12

--- server.rb
+++ server.rb
@@ -6,8 +6,8 @@ get '/teams' do
   content_type :json
   jsonArr = {"teams":[{"name":"OSC Lille", "code":"OSC",
                         "shortName":"Lille", "marketValue":"67,700,000 e",
-                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/img/clubs/lille.jpg"},
+                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/teams/img/clubs/lille.jpg"},
                       {"name":"Paris Saint Germain", "code":"PSG",
                         "shortName":"PSG", "marketValue":"408,550,000 e",
-                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/img/clubs/psg.png"}]}.to_json
+                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/teams/img/clubs/psg.png"}]}.to_json
 end
