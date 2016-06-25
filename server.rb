ce1f7549f8c1e3de342fb47c9b6b3aa1f86613b4
jgarus <javprg7@gmail.com>
2016-06-24 20:34:01 -0400

    Addin8

--- server.rb
+++ server.rb
@@ -5,15 +5,9 @@ require 'json'
 get '/teams' do
   content_type :json
   jsonArr = {"teams":[{"name":"OSC Lille", "code":"OSC",
-                        "shortname":"Lille", "marketValue":"67,700,000 e"},
+                        "shortname":"Lille", "marketValue":"67,700,000 e",
+                        "image":"/img/clubs/lille.jpg"},
                       {"name":"Paris Saint Germain", "code":"PSG",
-                        "shortname":"PSG", "marketValue":"408,550,000 e"}]}.to_json
-end
-
-get '/clubs' do
-  content_type :json
-  jsonArr = {"teams":[{"name":"OSC Lille", "code":"OSC",
-                        "shortname":"Lille", "marketValue":"67,700,000 e"},
-                      {"name":"Paris Saint Germain", "code":"PSG",
-                        "shortname":"PSG", "marketValue":"408,550,000 e"}]}.to_json
+                        "shortname":"PSG", "marketValue":"408,550,000 e",
+                        "image":"/img/clubs/psg.png"}]}.to_json
 end
