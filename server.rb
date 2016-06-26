e0de3717cb888c1064e2e9e6f66a480eefbb731b
jgarus <javprg7@gmail.com>
2016-06-26 14:42:38 -0400

    Creating images URL

--- server.rb
+++ server.rb
@@ -4,10 +4,15 @@ require 'json'
 
 get '/teams' do
   content_type :json
+  File.read(File.join('public', 'lille.jpg'))
   jsonArr = {"teams":[{"name":"OSC Lille", "code":"OSC",
                         "shortName":"Lille", "marketValue":"67,700,000 e",
-                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/teams/img/clubs/lille.jpg"},
+                        "imgUrl":""},
                       {"name":"Paris Saint Germain", "code":"PSG",
                         "shortName":"PSG", "marketValue":"408,550,000 e",
-                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/teams/img/clubs/psg.png"}]}.to_json
+                        "imgUrl":""}]}.to_json
+end
+
+get '/:filename' do
+  File.read(File.join('public', 'lille.jpg'))
 end
