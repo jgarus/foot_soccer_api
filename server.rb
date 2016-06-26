63d8ee1e0ce317a88d0c1cb2fc42ff2d682b96a6
jgarus <javprg7@gmail.com>
2016-06-26 14:44:19 -0400

    Fixing img URL

--- server.rb
+++ server.rb
@@ -7,10 +7,10 @@ get '/teams' do
   File.read(File.join('public', 'lille.jpg'))
   jsonArr = {"teams":[{"name":"OSC Lille", "code":"OSC",
                         "shortName":"Lille", "marketValue":"67,700,000 e",
-                        "imgUrl":""},
+                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/lille.jpg"},
                       {"name":"Paris Saint Germain", "code":"PSG",
                         "shortName":"PSG", "marketValue":"408,550,000 e",
-                        "imgUrl":""}]}.to_json
+                        "imgUrl":"https://dry-harbor-40455.herokuapp.com/psg.png"}]}.to_json
 end
 
 get '/:filename' do
