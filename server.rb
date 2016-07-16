af10f7b1aaf3d0e03d999bc63a92846d07d13fb1
Jesus <javprg7@gmail.com>
2016-07-16 18:07:52 -0400

    Added leagues JSON data and images

--- server.rb
+++ server.rb
@@ -10,12 +10,14 @@ get '/' do
   erb :index
 end
 
+# route to load the teams JSON data
 get '/teams' do
   content_type :json
   File.read('data/json/teams/ligue_1/ligue_1.json')
 end
 
+# route to load the leagues JSON data
 get '/leagues' do
   content_type :json
-  File.read('data/leagues/leagues.json')
+  File.read('data/json/leagues/leagues.json')
 end
